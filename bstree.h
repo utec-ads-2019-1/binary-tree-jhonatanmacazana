f373b8844ae15791ff700ee60adc4a6c7ff7577a
Jhonatan Macazana <jhonatan.macazana@utec.edu.pe>
2019-05-06 07:51:27 -0500

    segfault - ++it

--- bstree.h
+++ bstree.h
@@ -6,224 +6,159 @@
 
 template <typename T>
 class BSTree {
-    private:
-        Node<T> *root;
-        int nodes;
+private:
+    Node<T> *root;
+    int nodes;
 
-        void printPreOrder(Node<T>* node)
-        {
-            std::cout << node->data << std::endl;
-            if(node->left) printPreOrder(node->left);
-            if(node->right) printPreOrder(node->right);
-        }
+    void printPreOrder(Node<T>* node)
+    {
+        std::cout << node->data << std::endl;
+        if(node->left) printPreOrder(node->left);
+        if(node->right) printPreOrder(node->right);
+    }
 
-        void printInOrder(Node<T>* node)
-        {
-            if(node->left) printInOrder(node->left);
-            std::cout << node->data << std::endl;
-            if(node->right) printInOrder(node->right);
-        }
+    void printInOrder(Node<T>* node)
+    {
+        if(node->left) printInOrder(node->left);
+        std::cout << node->data << std::endl;
+        if(node->right) printInOrder(node->right);
+    }
 
-        void printPostOrder(Node<T>* node)
-        {
-            if(node->left) printPostOrder(node->left);
-            if(node->right) printPostOrder(node->right);
-            std::cout << node->data << std::endl;
-        }
+    void printPostOrder(Node<T>* node)
+    {
+        if(node->left) printPostOrder(node->left);
+        if(node->right) printPostOrder(node->right);
+        std::cout << node->data << std::endl;
+    }
 
-        bool swapVal(Node<T>* node1, Node<T>* node2)
-        {
-            T tempData;
-            tempData = node1->data;
-            node1->data = node2->data;
-            node2->data = tempData;
-            if(node2->left || node2->right) return false;
-            return true;
-        }
 
-        void kill(Node<T>* node, T data)
+    Node<T>* minValueNode(Node<T>* node)
+    {
+        auto currentNode = node;
+        while(currentNode && currentNode->left)
+            currentNode = currentNode->left;
+
+        return currentNode;
+    }
+
+    Node<T>* kill(Node<T>* node, T data)
+    {
+        if (!node) return node;
+
+        if (data < node->data)
+            node->left = kill(node->left, data);
+
+        else if (data > node->data)
+            node->right = kill(node->right, data);
+
+        else
         {
-            if (data == node->data)
+            if(!node->left)
             {
-                if(node->left)
-                {
-                    if(swapVal(node, node->left)){
-                        delete node->left;
-                        node->left = NULL;
-                    }
-                    else
-                        kill(node, data);
-                }
-                else if(node->right)
-                {
-                    if(swapVal(node, node->right)){
-                        delete node->right;
-                        node->right = NULL;
-                    }
-                    else
-                        kill(node, data);
-                }
-
+                auto tempNode = node->right;
+                free(node);
+                return tempNode;
             }
-            else if(data < node->data)
+
+            else if(!node->right)
             {
-                if(data == node->left->data)
-                {
-                    if(node->left->left)
-                    {
-                        if(swapVal(node->left, node->left->left)){
-                            delete node->left->left;
-                            node->left->left = NULL;
-                        }
-                        else
-                            kill(node->left->left, data);
-                    }
-                    else if (node->left->right)
-                    {
-                        if(swapVal(node->left, node->left->right)){
-                            delete node->left->right;
-                            node->left->right = NULL;
-                        }
-                        else
-                            kill(node->left->right, data);
-                    }
-                    else
-                    {
-                        delete node->left;
-                        node->left = NULL;
-                    }
-
-
-                }
-                else
-                    kill(node->left, data);
+                auto tempNode = node->left;
+                free(node);
+                return tempNode;
             }
-            else
-            {
-                if(data == node->right->data)
-                {
-                    if(node->right->left)
-                    {
-                        if(swapVal(node->right, node->right->left)){
-                            delete node->right->left;
-                            node->right->left = NULL;
-                        }
-                        else
-                            kill(node->right->left, data);
-                    }
-                    else if (node->right->right)
-                    {
-                        if(swapVal(node->right, node->right->right)){
-                            delete node->right->right;
-                            node->right->right = NULL;
-                        }
-                        else
-                            kill(node->right->right, data);
-                    }
-                    else
-                    {
-                        delete node->right;
-                        node->right = NULL;
-                    }
-                }
-                else
-                    kill(node->right, data);
-            }
+
+            auto tempNode = minValueNode(node->right);
+
+            node->data = tempNode->data;
+            node->right = kill(node->right, tempNode->data);
         }
+        return node;
 
-    public:
-        BSTree() : root(nullptr), nodes(0) {};
+    }
 
-        bool find(T data) {
-            Node<T> *searchNode = this->root;
+public:
+    BSTree() : root(nullptr), nodes(0) {};
 
+    bool find(T data) {
+        Node<T> *searchNode = this->root;
+
+        while(searchNode)
+        {
+            if(data < searchNode->data)
+            {
+                if(searchNode->left) searchNode = searchNode->left;
+                else break;
+            }
+            else if (data > searchNode->data)
+            {
+                if(searchNode->right) searchNode = searchNode->right;
+                else break;
+            }
+            else if(searchNode->data == data) return true;
+        }
+        return false;
+    }
+
+    bool insert(T data) {
+        Node<T> *searchNode = this -> root;
+        if (find(data)) return false;
+
+        if (!searchNode) this->root = new Node<T>(data);
+        else
+        {
             while(searchNode)
             {
                 if(data < searchNode->data)
                 {
                     if(searchNode->left) searchNode = searchNode->left;
-                    else break;
+                    else {searchNode->left = new Node<T>(data); break;}
                 }
-                else if (data > searchNode->data)
+                else
                 {
                     if(searchNode->right) searchNode = searchNode->right;
-                    else break;
-                }
-                else if(searchNode->data == data) return true;
-
-            }
-
-            return false;
-        }
-
-        bool insert(T data) {
-            Node<T> *searchNode = this -> root;
-            if (find(data)) return false;
-
-            if (!searchNode) this->root = new Node<T>(data);
-            else
-            {
-                while(searchNode)
-                {
-                    if(data < searchNode->data)
-                    {
-                        if(searchNode->left) searchNode = searchNode->left;
-                        else {searchNode->left = new Node<T>(data); break;}
-                    }
-                    else
-                    {
-                        if(searchNode->right) searchNode = searchNode->right;
-                        else {searchNode->right = new Node<T>(data); break;}
-                    }
+                    else {searchNode->right = new Node<T>(data); break;}
                 }
             }
-
-            ++nodes;
-            return true;
         }
+        ++nodes;
+        return true;
+    }
 
-        bool remove(T data) {
-            Node<T> *searchNode = this -> root;
-            if (!find(data) || !searchNode) return false;
-            kill(searchNode, data);
-            --nodes;
-            return true;
-        }
+    bool remove(T data) {
+        Node<T> *searchNode = this -> root;
+        if (!find(data) || !searchNode) return false;
+        this->root = kill(searchNode, data);
+        --nodes;
+        return true;
+    }
 
-        unsigned int size() {
-            return this->nodes;
-        }
+    unsigned int size() {
+        return this->nodes;
+    }
 
-        void print(Node<T>* node)
-        {
-            std::cout << node->data << std::endl;
-            if(node->left) print(node->left);
-            if(node->right) print(node->right);
-        }
+    void traversePreOrder() {
+        printPreOrder(this->root);
+    }
 
-        void traversePreOrder() {
-            printPreOrder(this->root);
-        }
+    void traverseInOrder() {
+        printInOrder(this->root);
+    }
 
-        void traverseInOrder() {
-            printInOrder(this->root);
-        }
+    void traversePostOrder() {
+        printPostOrder(this->root);
+    }
 
-        void traversePostOrder() {
-            printPostOrder(this->root);
-        }
+    Iterator<T> begin() {
+        return Iterator<T> (this->root);
+    }
 
-        Iterator<T> begin() {
-            return Iterator<T> (this->root);
-        }
+    Iterator<T> end() {
+        return Iterator<T> ();
+    }
 
-        Iterator<T> end() {
-            return Iterator<T> (NULL);
-        }
-
-        ~BSTree() {
-            // TODO
-        }
+    ~BSTree() {
+        this->root->killself(this->root);
+    }
 };
 
 #endif
--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-//#include "tester/tester.h"
+#include "tester/tester.h"
 
 #include "bstree.h"
 
@@ -10,42 +10,7 @@ int main(int argc, char *argv[]) {
     cout << "Binary Search Tree Practice" << endl;
     cout << "===========================================================" << endl << endl;
 
-    //Tester::execute();
-
-    BSTree<int> *tree = new BSTree<int>();
-
-
-    //cout << *(tree->root->data) << endl;
-    cout << "Hola" << endl;
-    cout << "Insert 17: " << tree->insert(17) << endl;
-    cout << "Insert 20: " << tree->insert(20) << endl;
-    cout << "Insert 10: " << tree->insert(10) << endl;
-    cout << "Insert 5: " << tree->insert(5) << endl;
-    cout << "Insert 2: " << tree->insert(2) << endl;
-    cout << "Insert 16: " << tree->insert(16) << endl;
-    cout << "Insert 13: " << tree->insert(13) << endl;
-    cout << "Insert 12: " << tree->insert(12) << endl;
-    cout << "Insert 15: " << tree->insert(15) << endl;
-    cout << "Size: " << tree->size() << endl;
-
-
-    tree->traverseInOrder();
-
-
-    Iterator<int> it = tree->begin();
-    cout << "Iterator: " << *it << endl;
-    ++it;
-    cout << "Iterator: " << *it << endl;
-    ++it;
-    cout << "Iterator: " << *it << endl;
-    ++it;
-    cout << "Iterator: " << *it << endl;
-    ++it;
-    cout << "Iterator: " << *it << endl;
-    ++it;
-    cout << "Iterator: " << *it << endl;
-    ++it;
-    cout << "Iterator: " << *it << endl;
+    Tester::execute();
 
     return EXIT_SUCCESS;
 }
